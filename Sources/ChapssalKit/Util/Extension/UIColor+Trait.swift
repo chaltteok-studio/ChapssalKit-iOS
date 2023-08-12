e49cd873684ddb45a5799db31eee636ab0d96d82
Jeong Jineun <wlsdms0122@gmail.com>
2023-08-12 18:17:08 +0900

    fix(): fit `UIColor+Trait` extension doesn't work on changes of user interface style

--- Sources/ChapssalKit/Util/Extension/UIColor+Trait.swift
+++ Sources/ChapssalKit/Util/Extension/UIColor+Trait.swift
@@ -1,6 +1,6 @@
 //
 //  UIColor+Trait.swift
-//  
+//
 //
 //  Created by JSilver on 2023/03/05.
 //
@@ -9,12 +9,14 @@ import UIKit
 
 public extension UIColor {
     static func traitColor(_ light: UIColor, dark: UIColor? = nil) -> UIColor {
-        switch UITraitCollection.current.userInterfaceStyle {
-        case .dark:
-            return dark ?? light
-            
-        default:
-            return light
+        .init { collection in
+            switch collection.userInterfaceStyle {
+            case .dark:
+                return dark ?? light
+                
+            default:
+                return light
+            }
         }
     }
 }
