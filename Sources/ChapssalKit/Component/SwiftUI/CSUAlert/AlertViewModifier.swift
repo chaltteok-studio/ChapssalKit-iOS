d9bd5274969e35a168a5c74964126dad236c6e55
Jeong Jineun <wlsdms0122@gmail.com>
2023-09-04 22:25:34 +0900

    fix(): Improvements to track changes to the scene corresponding to a alert view

--- Sources/ChapssalKit/Component/SwiftUI/CSUAlert/AlertViewModifier.swift
+++ Sources/ChapssalKit/Component/SwiftUI/CSUAlert/AlertViewModifier.swift
@@ -1,6 +1,6 @@
 //
-//  ErrorAlertViewModifier.swift
-//  
+//  AlertViewModifier.swift
+//
 //
 //  Created by JSilver on 2023/03/21.
 //
@@ -64,6 +64,8 @@ struct AlertViewModifier<Data, Alert: View>: ViewModifier {
     private var id: Namespace.ID
     @State
     private var isShow: Bool = false
+    @State
+    private var scene: UIWindowScene?
     
     // MARK: - Initializer
     init<P: Publisher>(
@@ -80,78 +82,82 @@ struct AlertViewModifier<Data, Alert: View>: ViewModifier {
     func body(content: Content) -> some View {
         content.background(
             ToastContainer { layer in
-                Group {
-                    if let window = layer.view?.window,
-                       let scene = window.windowScene {
-                        let queue = AlertQueue.queue(scene: scene)
-                        
-                        GeometryReader { reader in
-                            let frame = reader.frame(in: .global)
+                if let view = layer.view {
+                    Group {
+                        if let scene = scene {
+                            let queue = AlertQueue.queue(scene: scene)
                             
-                            Color.clear
-                                .toast(
-                                    $isShow,
-                                    duration: duration,
-                                    layouts: [
-                                        .inside(.top),
-                                        .inside(.trailing),
-                                        .inside(.bottom),
-                                        .inside(.leading)
-                                    ],
-                                    showAnimation: .fadeIn(duration: 0.2),
-                                    hideAnimation: .fadeOut(duration: 0.2),
-                                    hidden: { _ in
-                                        queue.check()
-                                    }
-                                ) {
-                                    if let data = queue.item?.data as? Data {
-                                        var completion: (() -> Void)?
-                                        
-                                        alert(data) {
-                                            completion = $0
-                                            
-                                            queue.remove()
-                                            queue.reset()
-                                        }
-                                            .onDisappear {
-                                                completion?()
-                                            }
-                                    }
-                                }
-                                    .offset(
-                                        x: -frame.minX,
-                                        y: -frame.minY
-                                    )
-                        }
-                            .frame(
-                                width: window.screen.bounds.width,
-                                height: window.screen.bounds.height
-                            )
-                            .subscribe(queue.$item) { item in
-                                guard item?.id == id else {
-                                    isShow = false
-                                    return
-                                }
+                            GeometryReader { reader in
+                                let frame = reader.frame(in: .global)
                                 
-                                isShow = (item?.data as? Data) != nil
+                                Color.clear
+                                    .toast(
+                                        $isShow,
+                                        duration: duration,
+                                        layouts: [
+                                            .inside(.top),
+                                            .inside(.trailing),
+                                            .inside(.bottom),
+                                            .inside(.leading)
+                                        ],
+                                        showAnimation: .fadeIn(duration: 0.2),
+                                        hideAnimation: .fadeOut(duration: 0.2),
+                                        hidden: { _ in
+                                            queue.check()
+                                        }
+                                    ) {
+                                        if let data = queue.item?.data as? Data {
+                                            var completion: (() -> Void)?
+                                            
+                                            alert(data) {
+                                                completion = $0
+                                                
+                                                queue.remove()
+                                                queue.reset()
+                                            }
+                                                .onDisappear {
+                                                    completion?()
+                                                }
+                                        }
+                                    }
+                                        .offset(
+                                            x: -frame.minX,
+                                            y: -frame.minY
+                                        )
                             }
-                    }
-                }
-                    .subscribe(publisher) { data in
-                        guard let scene = layer.view?.window?.windowScene else { return }
-                        
-                        let queue = AlertQueue.queue(scene: scene)
-                        
-                        Task {
-                            if queue.isEmpty {
-                                queue.insert(data, for: id)
-                                queue.check()
-                            } else {
-                                queue.insert(data, for: id)
-                                queue.reset()
-                            }
+                                .frame(
+                                    width: scene.screen.bounds.width,
+                                    height: scene.screen.bounds.height
+                                )
+                                .subscribe(queue.$item) { item in
+                                    guard item?.id == id else {
+                                        isShow = false
+                                        return
+                                    }
+                                    
+                                    isShow = (item?.data as? Data) != nil
+                                }
                         }
                     }
+                        .subscribe(view.publisher(for: \.window?.windowScene)) { scene in
+                            self.scene = scene
+                        }
+                        .subscribe(publisher) { data in
+                            guard let scene = scene else { return }
+                            
+                            let queue = AlertQueue.queue(scene: scene)
+                            
+                            Task {
+                                if queue.isEmpty {
+                                    queue.insert(data, for: id)
+                                    queue.check()
+                                } else {
+                                    queue.insert(data, for: id)
+                                    queue.reset()
+                                }
+                            }
+                        }
+                }
             }
         )
     }
@@ -182,89 +188,60 @@ public extension View {
 #if DEBUG
 struct AlertViewModifier_Preview: View {
     var body: some View {
-        GeometryReader { reader in
-            ScrollView(.horizontal) {
-                HStack {
-                    ZStack {
-                        Button("Show Alert") {
-                            alert.send(Void())
-                        }
-                            .buttonStyle(.borderedProminent)
-                            .fixedSize()
-                            .alert(countAlert) { count, dismiss in
-                                ZStack {
-                                    Color.black.opacity(0.6)
-                                    VStack {
-                                        Text("Count Alert \(count)")
-                                        Button("dismiss") {
-                                            dismiss(nil)
-                                        }
-                                        .buttonStyle(.borderedProminent)
-                                    }
-                                    .background(Color.white)
-                                }
-                            }
-                            .alert(alert) { _, dismiss in
-                                ZStack {
-                                    Color.black.opacity(0.6)
-                                    
-                                    VStack(spacing: 12) {
-                                        Text("Confirm Deletion")
-                                            .padding(6)
-                                            .font(Font(R.Font.font(ofSize: 20, weight: .medium)))
-                                            .foregroundColor(Color(uiColor: R.Color.gray01))
-                                        Text("Deleted data cannot be recovere.\nContinue?")
-                                            .multilineTextAlignment(.center)
-                                            .font(Font(R.Font.font(ofSize: 14, weight: .regular)))
-                                            .foregroundColor(Color(uiColor: R.Color.gray02))
-                                        HStack(spacing: 8) {
-                                            Button {
-                                                dismiss(nil)
-                                                /* Pefrom delete action */
-                                            } label: {
-                                                Text("Yes, Delete")
-                                                    .font(Font(R.Font.font(ofSize: 16, weight: .medium)))
-                                                    .foregroundColor(Color(uiColor: R.Color.gray04))
-                                                    .frame(maxWidth: .infinity)
-                                            }
-                                            Button {
-                                                dismiss(nil)
-                                            } label: {
-                                                Text("No, Cancel")
-                                                    .font(Font(R.Font.font(ofSize: 16, weight: .medium)))
-                                                    .foregroundColor(Color(uiColor: R.Color.green01))
-                                                    .frame(maxWidth: .infinity)
-                                            }
-                                        }
-                                        .padding(.vertical, 14)
-                                    }
-                                    .frame(width: 247)
-                                    .padding(16)
-                                    .background(Color(uiColor: R.Color.white))
-                                    .cornerRadius(10)
-                                }
-                            }
-                    }
-                        .frame(
-                            width: reader.size.width,
-                            height: reader.size.height
-                        )
+        Button("Show Alert") {
+            alert.send(Void())
+        }
+            .buttonStyle(.borderedProminent)
+            .fixedSize()
+            .alert(alert) { _, dismiss in
+                ZStack {
+                    Color.black.opacity(0.4)
                     
-                    Color.green
-                        .frame(
-                            width: reader.size.width,
-                            height: reader.size.height
-                        )
+                    VStack(spacing: 2) {
+                        VStack(spacing: 2) {
+                            Text("A Short Title Is Best")
+                                .font(.headline)
+                                .foregroundColor(.primary)
+                                .padding(.top, 21.5)
+                                .padding(.bottom, 2.5)
+                            Text("A message should be a short, complete sentence.")
+                                .multilineTextAlignment(.center)
+                                .font(.footnote)
+                                .foregroundColor(.primary)
+                                .padding(.top, 2.5)
+                                .padding(.bottom, 17.5)
+                        }
+                        VStack(spacing: 0) {
+                            Divider()
+                            Button {
+                                dismiss(nil)
+                            } label: {
+                                Text("Action")
+                                    .font(.system(size: 17, weight: .bold))
+                                    .foregroundColor(.blue)
+                                    .frame(height: 44)
+                                    .frame(maxWidth: .infinity)
+                            }
+                            Divider()
+                            Button {
+                                dismiss(nil)
+                            } label: {
+                                Text("Action")
+                                    .font(.system(size: 17, weight: .regular))
+                                    .foregroundColor(.blue)
+                                    .frame(height: 44)
+                                    .frame(maxWidth: .infinity)
+                            }
+                        }
+                    }
+                    .frame(width: 247)
+                    .background(.regularMaterial)
+                    .cornerRadius(14)
                 }
             }
-        }
     }
     
-    @State
-    var count = 0
-    
     let alert = PassthroughSubject<Void, Never>()
-    let countAlert = PassthroughSubject<Int, Never>()
 }
 
 struct AlertViewModifier_Previews: PreviewProvider {
